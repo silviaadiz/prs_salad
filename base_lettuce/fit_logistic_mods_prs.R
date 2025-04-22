da0d2731518bc6c504c1c68c70e0fca60e114aa0
Silvia Diz <61839435+silviaadiz@users.noreply.github.com>
2025-04-22 08:58:17 +0200

    Update fit_logistic_mods_prs.R
    clean code

--- base_lettuce/fit_logistic_mods_prs.R
+++ base_lettuce/fit_logistic_mods_prs.R
@@ -1,17 +1,30 @@
-fit_mod<-function(data,covar,outcomes){
-mod_prs<-function(p){f1<-as.formula(sprintf('%s ~ %s + st.prs',as.character(p),paste(covar,collapse="+")))
-glm(f1,data,family=binomial(link="logit"))}
-mod_quintil<-function(p){f2<-as.formula(sprintf('%s ~ %s + cincovsrest',as.character(p),paste(covar,collapse="+")))
-glm(f2,data,family=binomial(link="logit"))}
-    
-p<-outcomes
-models_prs<-map(p,~mod_prs(.x))
-models_quintil<-map(p,~mod_quintil(.x))
-    
-results_models<-bind_rows(
-map_dfr(models_prs,broom::tidy,.id="index"),
-map_dfr(models_quintil,broom::tidy,.id="index"))%>%
-filter(term%in%c("st.prs","cincovsrest"))%>%
-mutate(pheno=outcomes[as.numeric(index)],OR=exp(estimate),CI_l=exp(estimate-1.96*std.error),CI_u=exp(estimate+1.96*std.error))%>%
-select(-index)%>%relocate(pheno,.before=term)
-return(results_models)}
+fit_mod <- function(data, covar, outcomes) {
+ 
+# Define formulas
+    form_prs <- function(pheno) {
+    as.formula(sprintf('%s ~ %s + st.prs', pheno, paste(covar, collapse = "+")))
+  }
+  
+  form_quintile <- function(pheno) {
+    as.formula(sprintf('%s ~ %s + quintile', pheno, paste(covar, collapse = "+")))
+  }
+  
+  # Fit models
+  models_prs <- map(outcomes, ~glm(form_prs(.x), data = data, family = binomial))
+  models_quintile <- map(outcomes, ~glm(form_quintile(.x), data = data, family = binomial))
+  
+  # Build results
+  bind_rows(
+    map_dfr(models_prs, broom::tidy, conf.int = TRUE, .id = "model_index"), 
+    map_dfr(models_quintile, broom::tidy, conf.int = TRUE, .id = "model_index")
+  ) %>%
+    filter(term %in% c("st.prs", "quintile")) %>%
+    mutate(
+      pheno = outcomes[as.numeric(model_index)],
+      OR = exp(estimate),
+      CI_l = exp(conf.low),  
+      CI_u = exp(conf.high)
+    ) %>%
+    select(-model_index) %>%
+    relocate(pheno, .before = term)
+}
